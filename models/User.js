4618c6897feb641a3f4fa6644a50b835c36dbd7b
frankda <suyunyi@gmail.com>
2021-06-05 18:01:50 +1000

    - Add compare password function - Fix hash password generate issue in mongoose

--- models/User.js
+++ models/User.js
@@ -28,12 +28,21 @@ userSchema.pre("save", function save(next) {
     const user = this;
     if (!user.isModified("password")) { return next(); }
     bcrypt.genSalt(10, (err, salt) => {
-        if (err) { return next(err); }
-        bcrypt.hash(user.password, salt, undefined, (err, hash) => {
-            if (err) { return next(err); }
-            user.password = hash;
-            next();
-        });
+      if (err) { return next(err); }
+      bcrypt.hash(user.password, salt, (err, hash) => {
+          if (err) { return next(err); }
+          user.password = hash;
+          next();
+      });
     });
 })
+
+const comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+userSchema.methods.comparePassword = comparePassword;
+
 export const User = mongoose.model("User", userSchema);
